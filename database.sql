1b5f5f73b00fcc2db455557419f2b9fff09102c9
katlegomyra91 <katlego@myramade.co.za>
2022-04-14 23:18:32 +0200

    add database file

---
+++ database.sql
@@ -0,0 +1,38 @@
+CREATE TABLE `status` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `status_name` varchar(50) NOT NULL,
+    `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `last_modified` datetime DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`)
+);
+
+INSERT INTO status (status_name)
+VALUES ('active'),
+('inactive'),
+('deleted'),
+('marked');
+
+CREATE TABLE `list` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `list_name` varchar(200) NOT NULL,
+    `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `last_modified` datetime DEFAULT CURRENT_TIMESTAMP,
+    `status_id` int NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `status_id` (`status_id`),
+    CONSTRAINT `list_status_id` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`)
+);
+
+CREATE TABLE `list_item` (
+    `id` int NOT NULL AUTO_INCREMENT,
+    `item_name` varchar(200) NOT NULL,
+    `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `last_modified` datetime DEFAULT CURRENT_TIMESTAMP,
+    `list_id` int NOT NULL,
+    `status_id` int NOT NULL,
+    PRIMARY KEY (`id`),
+    KEY `list_id` (`list_id`),
+    KEY `status_id` (`status_id`),
+    CONSTRAINT `list_item_list_id` FOREIGN KEY (`list_id`) REFERENCES `list` (`id`),
+    CONSTRAINT `list_item_status_id` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`)
+);
